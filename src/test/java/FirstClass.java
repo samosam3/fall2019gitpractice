208b5dd783cdfcad86799111fbd5e80b872e6b8d
samosam3 <usama.nw00@yahoo.com>
2020-04-08 18:16:32 -0400

    add practice.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,5 +2,6 @@ public class FirstClass {
     public static void main(String[] args) {
 
         System.out.println("brave new world");
+        System.out.println("ola seniour");
     }
 }
---
+++ src/test/java/practice.java
@@ -0,0 +1,12 @@
+public class practice {
+    public static void main(String[] args) {
+
+
+        System.out.println("just practicing  ");
+        System.out.println(" yes its practicing ");
+
+
+
+
+    }
+}
