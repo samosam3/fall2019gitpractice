6a657d8ad361c36e0491404d53cfe8a92fc5bb3f
samosam3 <58535630+samosam3@users.noreply.github.com>
2020-04-10 16:59:20 -0400

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,6 +2,6 @@ public class FirstClass {
     public static void main(String[] args) {
 
         System.out.println("brave new world");
-        System.out.println("ola seniour");
+        System.out.println("ola habib");
     }
 }
